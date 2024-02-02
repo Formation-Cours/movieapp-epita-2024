af3b2f9d94c2c48e272399660beddc0470e54232
user_name <example.gmail.com>
2024-02-02 10:35:14 +0100

    first commit

---
+++ src/main/java/com/formation/movieappback/MovieappBackApplication.java
@@ -0,0 +1,13 @@
+package com.formation.movieappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieappBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieappBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/formation/movieappback/MovieappBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.movieappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieappBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
